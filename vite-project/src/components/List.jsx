d79c3bbb748d3a21738559b00b1eaae3d43df182
shriffe-noureddine <noureddineshriffe@gmail.com>
2025-02-25 15:37:47 +0100

    pass data to components

---
+++ vite-project/src/components/List.jsx
@@ -0,0 +1,10 @@
+import Post from './Post'
+
+function List(){
+    return <ul>
+        <Post name="Noureddine" body="This is my first custom component in React"></Post>
+        <Post name="Nahla" body="Hello World"></Post>
+    </ul>
+}
+
+export default List;
---
+++ vite-project/src/components/Post.jsx
@@ -0,0 +1,8 @@
+function Post(props){
+    return <div>
+        <h1>{props.name}</h1>
+        <p>{props.body}</p>
+    </div>
+}
+
+export default Post;
